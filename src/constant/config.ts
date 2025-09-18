f2eabb47cc1126e4064b4e46edac358f3e879985
Wagyu <101499843+0xwagyu-eth@users.noreply.github.com>
2025-09-18 12:14:50 +1000

    final2

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  title: 'Cakeshop',
+  title: 'Cakeshop | BTC as easy as CAKE',
   description: 'Buying BTC should be easy as cake',
   url: 'https://fc.vercel.app',
 };
