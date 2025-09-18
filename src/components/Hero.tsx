9406e5e4158bdbffcd2abe9a8db0f2831bfac28f
Wagyu <101499843+0xwagyu-eth@users.noreply.github.com>
2025-09-18 12:42:47 +1000

    finalw

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,23 +5,36 @@ import RegisterForm from '@/components/RegisterForm';
 export default function Hero() {
   return (
     <section className='min-h-screen flex items-center justify-center p-2 md:p-2 pt-20 md:pt-2'>
-      <div className='border border-white/10 text-white rounded-3xl shadow-2xl w-full overflow-hidden p-2 md:p-6 transition-all duration-300' style={{ background: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)' }}>
+      <div
+        className='border border-white/10 text-white rounded-3xl shadow-2xl w-full overflow-hidden p-2 md:p-6 transition-all duration-300'
+        style={{
+          background: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)',
+        }}
+      >
         <div className='flex flex-col md:flex-row items-center justify-between px-6 py-12 md:py-16 gap-8 md:gap-12'>
           <div className='md:w-3/5 text-center md:text-left mb-8 md:mb-0'>
             <div className='flex items-center justify-center md:justify-start mb-8'>
-              <Image src='/cake4.png' alt="Cakeshop Logo" className="w-8 h-8 text-white mr-3" width={32} height={32} />
-              <h2 className='text-2xl md:text-3xl font-bold text-primary-300 text-shadow-sm'>Cakeshop App</h2>
-
+              <Image
+                src='/cake4.png'
+                alt='Cakeshop Logo'
+                className='w-8 h-8 text-white mr-3'
+                width={32}
+                height={32}
+              />
+              <h2 className='text-2xl md:text-3xl font-bold text-primary-300 text-shadow-sm'>
+                Cakeshop App
+              </h2>
             </div>
             <h1 className='text-4xl md:text-6xl lg:text-7xl font-bold leading-tight mb-6 text-white text-shadow'>
-              Buying BTC should be <span className='text-primary-300'>as easy as</span> cake
+              Buying BTC should be{' '}
+              <span className='text-primary-300'>as easy as cake</span>
             </h1>
             <p className='text-lg md:text-xl mb-10 text-white/90 leading-relaxed'>
               Buy Bitcoin. Earn Yield. Build Wealth.
             </p>
             <RegisterForm />
           </div>
-          <div className='hidden md:flex md:w-2/5 justify-center items-end mt-auto md:mt-0 pb-2 sm:pb-4'>
+          <div className='hidden md:flex md:w-2/5 justify-center items-end mt-auto md:mt-0 pb-8 sm:pb-12'>
             <div className='relative w-full max-w-[28rem] h-[28rem] sm:h-[28rem] md:h-[32rem] group'>
               <div className='absolute -inset-10 bg-white/10 backdrop-blur-xl rounded-[2rem] blur-2xl opacity-50 group-hover:opacity-75 transition-opacity duration-500 pointer-events-none'></div>
 
